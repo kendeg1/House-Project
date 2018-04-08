903be7528e03d5f4264e8f995c9ee60a2eb21f18
kendeg1 <kendeg1@gmail.com>
2018-04-08 19:16:09 -0400

    edited script.js and houseSearch.jsp

--- temp/WebContent/script.js
+++ temp/WebContent/script.js
@@ -8,3 +8,12 @@ $('#mycheckbox').change(function(){
                 $("mycheckboxdiv").next().hide();
             }
         });
+$(document).ready(function(){
+    $('#sell').change(function(){
+        if(this.checked)
+            $('#duration').fadeIn('slow');
+        else
+            $('#duration').fadeOut('slow');
+
+    });
+});
